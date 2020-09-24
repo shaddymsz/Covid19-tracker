045651ac09f81341b5ae4021bbfb422b719bc9a3
shaddymsz <shad.zaidi2@gmail.com>
2020-09-24 15:17:20 +0530

    Covide 19 tracker live

---
+++ src/Map.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Map.css";
+import { Map as LeafletMap, TileLayer, TitleLayer } from "react-leaflet";
+import { showDataOnMap} from "./utl";
+
+function Map({countries,casesType, center, zoom}) {
+    return (
+        <div className="map">
+            <LeafletMap center={center} zoom={zoom}>
+                <TileLayer
+                url = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://osm.org/copyright">
+                OpenStreetMap</a> contributors'/> 
+                {showDataOnMap(countries, casesType)}
+            </LeafletMap>
+           
+        </div>
+    )
+}
+
+export default Map
